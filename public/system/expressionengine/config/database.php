4e1fcd9a70e9862591de0c3b6f9970a04946e1fd
johnnyfyve <johnnyfyve@gmail.com>
2013-08-31 19:51:27 -0400

    adding all source files

---
+++ public/system/expressionengine/config/database.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$active_group = 'expressionengine';
+$active_record = TRUE;
+
+$db['expressionengine']['hostname'] = 'localhost';
+$db['expressionengine']['username'] = 'jdl_site';
+$db['expressionengine']['password'] = 'Jmie05!!';
+$db['expressionengine']['database'] = 'jdl_site';
+$db['expressionengine']['dbdriver'] = 'mysql';
+$db['expressionengine']['pconnect'] = FALSE;
+$db['expressionengine']['dbprefix'] = 'exp_';
+$db['expressionengine']['swap_pre'] = 'exp_';
+$db['expressionengine']['db_debug'] = TRUE;
+$db['expressionengine']['cache_on'] = FALSE;
+$db['expressionengine']['autoinit'] = FALSE;
+$db['expressionengine']['char_set'] = 'utf8';
+$db['expressionengine']['dbcollat'] = 'utf8_general_ci';
+$db['expressionengine']['cachedir'] = '/users/jonathanking/Sites/jdl-site/public/system/expressionengine/cache/db_cache/';
+
+/* End of file database.php */
+/* Location: ./system/expressionengine/config/database.php */
